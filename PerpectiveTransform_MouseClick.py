ce6cc5bc6bf9762e89f2bdc0edcda818a6aaebbe
kim0930 <54169759+kim0930@users.noreply.github.com>
2023-02-12 21:04:48 +0900

    Update PerpectiveTransform_MouseClick.py

--- PerpectiveTransform_MouseClick.py
+++ PerpectiveTransform_MouseClick.py
@@ -1,3 +1,10 @@
+import cv2
+import numpy as np
+
+win_name = "scanning"
+img = cv2.imread("imgs/paper.jpg")
+rows, cols = img.shape[:2]
+draw = img.copy()
 pts_cnt = 0
 pts = np.zeros((4, 2), dtype=np.float32)
 
